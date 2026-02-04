6531ae2370102ddd9d7ba8c9837cb1b7c0aa1d25
Gabriel Delgado Pérez <19gdp97@gmail.com>
2026-02-04 11:53:24 +0100

    feat: login and protect board with auth guard

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import BoardPage from "../pages/BoardPage";
+import ProtectedRoute from "./ProtectedRoute";
+
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/board" element={<BoardPage />} />
+        <Route path="/board" element={ <ProtectedRoute> <BoardPage /> </ProtectedRoute> } />
+
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+
 function LoginPage() {
-  return <h2>Login</h2>;
+  const [username, setUsername] = useState("");
+  const login = useAuthStore((state) => state.login);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (!username.trim()) return;
+    login(username);
+    navigate("/board");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 }
 
 export default LoginPage;
+
